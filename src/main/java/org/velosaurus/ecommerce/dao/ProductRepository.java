ac2f3ea89ad043a841e6cfba8ac7ca0d056fef5a
Oliver Zott <zott_oliver@web.de>
2021-11-02 18:03:01 +0100

    Query Method for REST API added.

--- src/main/java/org/velosaurus/ecommerce/dao/ProductRepository.java
+++ src/main/java/org/velosaurus/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,15 @@
 package org.velosaurus.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.velosaurus.ecommerce.entity.Product;
 
 // cross-origin to accept calls from web browser scripts for this origin
 @CrossOrigin("http://localhost:4200/")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
